f77a00f9e02e6650e3d011d4b68ef59b31872378
Ezequiel Pássaro <epassaro15@gmail.com>
2025-07-19 22:44:01 -0300

    Fix pandas version; Update script

--- make_atomic_file.py
+++ make_atomic_file.py
@@ -1,9 +1,11 @@
 from carsus import init_db
 from carsus.io.nist import NISTWeightsCompIngester, NISTIonizationEnergiesIngester
 from carsus.io.kurucz import GFALLIngester
+from carsus.io.chianti_ import ChiantiIngester
+from carsus.io.zeta import KnoxLongZetaIngester
+from carsus.io.output import AtomData
 
-from carsus.model import Atom
-
+# Init database
 session = init_db()
 
 # Basic atomic data
@@ -15,11 +17,23 @@ weightscomp_ingester.ingest()
 session.commit()
 
 # Ionization energies and ground levels (NIST)
-ioniz_energies_ingester = NISTIonizationEnergiesIngester(session, spectra="Si")
-ioniz_energies_ingester.ingest(ionization_energies=True, ground_levels=True)
-session.commit()
+#ioniz_energies_ingester = NISTIonizationEnergiesIngester(session, spectra="Si")
+#ioniz_energies_ingester.ingest(ionization_energies=True, ground_levels=True)
+#session.commit()
 
-# Levels and lines (Kurucz)
-#gfall_ingester = GFALLIngester(session, fname="./gfall.dat", ions="Si 1-2")
+# H-Zn levels and lines (Kurucz)
+#gfall_ingester = GFALLIngester(session, fname="./docs/gfall.dat", ions="H-Zn")
 #gfall_ingester.ingest(levels=True, lines=False)
 #session.commit()
+
+# Si I-II levels and lines (Chianti)
+#chianti_ingester = ChiantiIngester(session, ions="Si 1-2")
+#chianti_ingester.ingest(levels=True, lines=True)
+#session.commit()
+
+# Zeta data (Knox Long)
+zeta_ingester = KnoxLongZetaIngester(session, './carsus/data/knox_long_recombination_zeta.dat')
+zeta_ingester.ingest()
+session.commit()
+
+atom_data = AtomData(session, selected_atoms="Si", chianti_short_name='chianti_v9.0', chianti_ions="Si 1-2")z
